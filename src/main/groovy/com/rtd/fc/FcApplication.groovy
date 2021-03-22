960e1246f6b424ba83b5089374682345392d5c35
Octavio Luna <octavio.luna@gmail.com>
2021-03-22 13:04:05 -0500

    #1 creating empty place holder

---
+++ src/main/groovy/com/rtd/fc/FcApplication.groovy
@@ -0,0 +1,13 @@
+package com.rtd.fc
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FcApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FcApplication, args)
+	}
+
+}
